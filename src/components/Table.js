1a0e21caca95d811e0aea1029ad39f34e8bab573
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-15 14:50:43 +0530

    update table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,47 +1,34 @@
 import React from "react";
-import { DataGrid } from "@material-ui/data-grid";
+import MUIDataTable from "mui-datatables";
+
+const columns = [
+  "State/UT",
+  "Confirmed",
+  "Active",
+  "Recovered",
+  "Deceased",
+  "Tested",
+];
+
+const data = [
+  ["Joe James", "Test Corp", "Yonkers", "NY"],
+  ["John Walsh", "Test Corp", "Hartford", "CT"],
+  ["Bob Herm", "Test Corp", "Tampa", "FL"],
+  ["James Houston", "Test Corp", "Dallas", "TX"],
+];
 
 function Table() {
-  const columns = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "firstName", headerName: "First name", width: 130 },
-    { field: "lastName", headerName: "Last name", width: 130 },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 90,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.getValue("firstName") || ""} ${
-          params.getValue("lastName") || ""
-        }`,
-    },
-  ];
-
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  ];
-
-  return (
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid rows={rows} columns={columns} />
-    </div>
-  );
+  const options = {
+    filterType: "checkbox",
+    pagination: false,
+    print: false,
+    download: false,
+    filter: false,
+    search: false,
+    viewColumns: false,
+    selectableRows: "none",
+  };
+  return <MUIDataTable data={data} columns={columns} options={options} />;
 }
 
 export default Table;
