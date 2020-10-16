d376b7b454ee15d404be50f27ae825b3a25d5629
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-16 16:14:08 +0530

    update table to use api

--- src/components/Table2.js
+++ src/components/Table2.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import PropTypes from "prop-types";
 import clsx from "clsx";
 import { lighten, makeStyles } from "@material-ui/core/styles";
@@ -16,25 +17,28 @@ import Paper from "@material-ui/core/Paper";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
 
+import { STATE_NAMES } from "../constants";
+import api from "../api/covid19india";
+
 function createData(name, confirmed, active, recovered, deceased) {
   return { name, confirmed, active, recovered, deceased };
 }
 
-const rows = [
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Honeycomb", 408, 3.2, 87, 6.5),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  createData("KitKat", 518, 26.0, 65, 7.0),
-  createData("Lollipop", 392, 0.2, 98, 0.0),
-  createData("Marshmallow", 318, 0, 81, 2.0),
-  createData("Nougat", 360, 19.0, 9, 37.0),
-  createData("Oreo", 437, 18.0, 63, 4.0),
-];
+// const rows = [
+//   createData("Cupcake", 305, 3.7, 67, 4.3),
+//   createData("Donut", 452, 25.0, 51, 4.9),
+//   createData("Eclair", 262, 16.0, 24, 6.0),
+//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+//   createData("Gingerbread", 356, 16.0, 49, 3.9),
+//   createData("Honeycomb", 408, 3.2, 87, 6.5),
+//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+//   createData("Jelly Bean", 375, 0.0, 94, 0.0),
+//   createData("KitKat", 518, 26.0, 65, 7.0),
+//   createData("Lollipop", 392, 0.2, 98, 0.0),
+//   createData("Marshmallow", 318, 0, 81, 2.0),
+//   createData("Nougat", 360, 19.0, 9, 37.0),
+//   createData("Oreo", 437, 18.0, 63, 4.0),
+// ];
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -217,6 +221,7 @@ export default function EnhancedTable() {
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(true);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rows, setRows] = React.useState([]);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === "asc";
@@ -268,6 +273,35 @@ export default function EnhancedTable() {
 
   const emptyRows =
     rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  useEffect(() => {
+    const getTableData = async () => {
+      const res = await api.get("/v4/data.json");
+      console.log(res.data);
+      const formatData = [];
+      for (const state in res.data) {
+        formatData.push([
+          STATE_NAMES[state],
+          res.data[state].total.confirmed,
+          res.data[state].total.confirmed -
+            res.data[state].total.recovered -
+            res.data[state].total.deceased,
+          res.data[state].total.recovered,
+          res.data[state].total.deceased,
+          res.data[state].total.tested,
+        ]);
+      }
+      const filterData = formatData.filter((arr) => arr[0] !== "India");
+      console.log(filterData);
+      setRows(
+        filterData.map((arr) => {
+          return createData(arr[0], arr[1], arr[2], arr[3], arr[4]);
+        })
+      );
+      // setData(formatData.filter((arr) => arr[0] !== "India"));
+    };
+    getTableData();
+  }, []);
 
   return (
     <div className={classes.root}>
