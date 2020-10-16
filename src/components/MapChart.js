ad02ecab0b039e7b6f1feeb7e8dc2b1d52279841
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-16 23:16:34 +0530

    use example from react-simple-maps

---
+++ src/components/MapChart.js
@@ -0,0 +1,64 @@
+import React, { memo } from "react";
+import {
+  ZoomableGroup,
+  ComposableMap,
+  Geographies,
+  Geography,
+} from "react-simple-maps";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+const rounded = (num) => {
+  if (num > 1000000000) {
+    return Math.round(num / 100000000) / 10 + "Bn";
+  } else if (num > 1000000) {
+    return Math.round(num / 100000) / 10 + "M";
+  } else {
+    return Math.round(num / 100) / 10 + "K";
+  }
+};
+
+const MapChart = ({ setTooltipContent }) => {
+  return (
+    <>
+      <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
+        <ZoomableGroup>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    const { NAME, POP_EST } = geo.properties;
+                    setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                  style={{
+                    default: {
+                      fill: "#D6D6DA",
+                      outline: "none",
+                    },
+                    hover: {
+                      fill: "#F53",
+                      outline: "none",
+                    },
+                    pressed: {
+                      fill: "#E42",
+                      outline: "none",
+                    },
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </>
+  );
+};
+
+export default memo(MapChart);
