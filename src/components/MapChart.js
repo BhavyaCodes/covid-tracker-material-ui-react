0ba40ef4fd4f889a0c60f23319085338bd963e7b
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-17 15:43:08 +0530

    map working, offset needs to be fixed

--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -1,4 +1,5 @@
 import React, { memo } from "react";
+import { geoMercator, geoCentroid } from "d3-geo";
 import {
   ZoomableGroup,
   ComposableMap,
@@ -6,56 +7,52 @@ import {
   Geography,
 } from "react-simple-maps";
 
+import indiaMap from "../maps/india-map-2.json";
+
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
-
-const rounded = (num) => {
-  if (num > 1000000000) {
-    return Math.round(num / 100000000) / 10 + "Bn";
-  } else if (num > 1000000) {
-    return Math.round(num / 100000) / 10 + "M";
-  } else {
-    return Math.round(num / 100) / 10 + "K";
-  }
-};
 
 const MapChart = ({ setTooltipContent }) => {
   return (
     <>
-      <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
-        <ZoomableGroup>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  onMouseEnter={() => {
-                    const { NAME, POP_EST } = geo.properties;
-                    setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
-                  }}
-                  onMouseLeave={() => {
-                    setTooltipContent("");
-                  }}
-                  style={{
-                    default: {
-                      fill: "#D6D6DA",
-                      outline: "none",
-                    },
-                    hover: {
-                      fill: "#F53",
-                      outline: "none",
-                    },
-                    pressed: {
-                      fill: "#E42",
-                      outline: "none",
-                    },
-                  }}
-                />
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
+      <ComposableMap
+        data-tip=""
+        projection={geoMercator()}
+        projectionConfig={{
+          scale: 200,
+        }}
+      >
+        <Geographies geography={indiaMap}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                onMouseEnter={() => {
+                  const { st_nm, st_code } = geo.properties;
+                  setTooltipContent(`${st_nm} — ${st_code}`);
+                }}
+                onMouseLeave={() => {
+                  setTooltipContent("");
+                }}
+                style={{
+                  default: {
+                    fill: "#D6D6DA",
+                    outline: "none",
+                  },
+                  hover: {
+                    fill: "#F53",
+                    outline: "none",
+                  },
+                  pressed: {
+                    fill: "#E42",
+                    outline: "none",
+                  },
+                }}
+              />
+            ))
+          }
+        </Geographies>
       </ComposableMap>
     </>
   );
