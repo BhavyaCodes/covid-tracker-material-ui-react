3eb3d19a8be882666e3bba6fa572a47fd71ce545
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-18 17:35:05 +0530

    add getMaxValue function to MapChart

--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -1,10 +1,43 @@
-import React, { memo } from "react";
-import red from "@material-ui/core/colors/red";
+import React, { memo, useContext } from "react";
+import _ from "lodash";
+// import red from "@material-ui/core/colors/red";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 import indiaMap from "../maps/india-map-2.json";
+import { DataContext } from "../context/data.context";
+import { AttributeContext } from "../context/attribute.context";
 
 const MapChart = ({ setTooltipContent }) => {
+  const data = useContext(DataContext);
+  const attribute = useContext(AttributeContext);
+  if (!data.hasLoaded) {
+    return null;
+  }
+  data.data = _.omit(data.data, ["TT"]);
+
+  const getMaxValue = () => {
+    let max = 0;
+    if (attribute === "active") {
+      for (const state in data.data) {
+        let active =
+          data.data[state]["total"]["confirmed"] -
+          data.data[state]["total"]["recovered"] -
+          data.data[state]["total"]["deceased"];
+        if (active > max) {
+          max = active;
+        }
+      }
+    } else {
+      for (const state in data.data) {
+        if (data.data[state]["total"][attribute] > max) {
+          max = data.data[state]["total"][attribute];
+        }
+      }
+    }
+
+    return max;
+  };
+
   return (
     <>
       <ComposableMap
