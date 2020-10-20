3763820d1a6099e30792d105d53c5e6e89263f06
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-20 16:58:20 +0530

    display data on hover

--- src/components/MapHeader.js
+++ src/components/MapHeader.js
@@ -2,10 +2,12 @@ import React, { useContext } from "react";
 
 import Typography from "@material-ui/core/Typography";
 import { DataContext } from "../context/data.context";
+import { AttributeContext } from "../context/attribute.context";
 import { STATE_NAMES } from "../constants";
 
 function MapHeader({ locationId }) {
   const data = useContext(DataContext);
+  const attribute = useContext(AttributeContext);
 
   if (!data.hasLoaded) {
     return null;
@@ -14,6 +16,12 @@ function MapHeader({ locationId }) {
     <div>
       <Typography variant="h4" gutterBottom>
         {STATE_NAMES[locationId]}
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        {data.data[locationId]["total"][attribute]}
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        {attribute}
       </Typography>
     </div>
   );
