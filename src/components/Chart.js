d36d4b2e89230b18323c868adb46110063ce633c
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-21 12:29:12 +0530

    scaffolding components

---
+++ src/components/Chart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Chart() {
+  return <div>Chart</div>;
+}
+
+export default Chart;
---
+++ src/components/ChartList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Chart from "./Chart.js";
+
+function ChartList() {
+  return (
+    <div>
+      <Chart />
+    </div>
+  );
+}
+
+export default ChartList;
