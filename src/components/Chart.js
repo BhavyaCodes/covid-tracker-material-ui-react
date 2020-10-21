4391b7071dc86dd1eb4368c0346a8b8e7ad4368f
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-21 21:48:57 +0530

    display only last three months info

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,35 +1,82 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 
-function Chart({ data }) {
+function Chart({ data, attribute = "confirmed" }) {
   console.log(data);
-  // const buildChartData = (data, casesType) => {
-  // 	let chartData = [];
-  // 	let lastDataPoint;
-  // 	for (let date in data.cases) {
-  // 		if (lastDataPoint) {
-  // 			let newDataPoint = {
-  // 				x: date,
-  // 				y: data[casesType][date] - lastDataPoint,
-  // 			};
-  // 			chartData.push(newDataPoint);
-  // 		}
-  // 		lastDataPoint = data[casesType][date];
-  // 	}
-  // 	return chartData;
-  // };
+  console.log(attribute);
+  const buildChartData = (duration) => {
+    let chartData = [];
+    if (duration === "all") {
+      for (let date in data["MH"].dates) {
+        let newDataPoint = {
+          x: date,
+          y: data["MH"]["dates"][date]["total"]["confirmed"],
+        };
+        chartData.push(newDataPoint);
+      }
+      return chartData;
+    } else if (duration === "3") {
+      const dateDuration = new Date().getTime() - 1000 * 60 * 60 * 24 * 90;
+      for (let date in data["MH"].dates) {
+        // const dateToday = new Date();
+        const dataDate = new Date(
+          date.split("-")[0],
+          date.split("-")[1],
+          date.split("-")[2]
+        );
+        if (dateDuration - dataDate.getTime() <= 0) {
+          let newDataPoint = {
+            x: date,
+            y: data["MH"]["dates"][date]["total"]["confirmed"],
+          };
+          chartData.push(newDataPoint);
+        }
+      }
+      return chartData;
+    }
+  };
+
+  console.log("buildChartData", buildChartData("3"));
+
+  const options = {
+    scales: {
+      xAxes: [
+        {
+          type: "time",
+          time: {
+            format: "YY-MM-DD",
+            tooltipFormat: "ll",
+          },
+        },
+      ],
+      yAxes: [
+        {
+          gridLines: {
+            display: false,
+          },
+          ticks: {
+            // Include a dollar sign in the ticks
+            callback: function (value, index, values) {
+              return value;
+            },
+          },
+        },
+      ],
+    },
+  };
 
   return (
     <Line
       data={{
-        labels: ["red", "blue", "yellow", "green"],
+        // labels: ["red", "blue", "yellow", "green"],
         datasets: [
           {
             label: "confirmed total cases in maharashtra",
-            data: [12, 9, 3, 4, 5],
+            data: buildChartData("3"),
           },
         ],
       }}
+      options={options}
     />
   );
 }
