783a6f955dd82f99ada23fb8ede8ee169e5f3406
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-22 16:52:02 +0530

    add deceased and recovered charts

--- src/components/ChartList.js
+++ src/components/ChartList.js
@@ -23,6 +23,24 @@ function ChartList() {
         borderColor="#CC1034"
         backgroundColor="rgba(204, 16, 52, 0.5)"
       />
+      <Chart
+        data={historicalData.data}
+        state={state}
+        duration={duration}
+        attribute="deceased"
+        type={type}
+        borderColor="grey"
+        backgroundColor="rgba(186, 186, 186, 0.5)"
+      />
+      <Chart
+        data={historicalData.data}
+        state={state}
+        duration={duration}
+        attribute="recovered"
+        type={type}
+        borderColor="green"
+        backgroundColor="rgba(40, 247, 17, 0.5)"
+      />
     </div>
   );
 }
