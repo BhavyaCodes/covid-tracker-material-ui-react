de47cc3775afb881bfb3ea722d4bf796c7c5dc8f
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-22 18:09:17 +0530

    add region selector

--- src/components/ChartList.js
+++ src/components/ChartList.js
@@ -1,19 +1,63 @@
 import React, { useContext, useState } from "react";
 import Chart from "./Chart.js";
 
+import { makeStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
 import { HistoricalData } from "../context/historicalData.context";
+import { STATE_NAMES } from "../constants";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function ChartList() {
+  const classes = useStyles();
   const historicalData = useContext(HistoricalData);
-  const [state, setState] = useState("GJ");
+  const [state, setState] = useState("TT");
   const [duration, setDuration] = useState(3);
   const [type, setType] = useState("total");
+
+  const handleChange = (event) => {
+    setState(event.target.value);
+  };
 
   if (!historicalData.hasLoaded) {
     return null;
   }
+  console.log(historicalData);
+  const renderRegionOptions = () => {
+    return Object.keys(historicalData.data).map((stateId) => {
+      if (STATE_NAMES[stateId]) {
+        return (
+          <MenuItem key={stateId} value={stateId}>
+            {STATE_NAMES[stateId]}
+          </MenuItem>
+        );
+      }
+      return null;
+    });
+  };
   return (
     <div>
+      <FormControl className={classes.formControl}>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={state}
+          onChange={handleChange}
+        >
+          {renderRegionOptions()}
+        </Select>
+      </FormControl>
       <Chart
         data={historicalData.data}
         state={state}
