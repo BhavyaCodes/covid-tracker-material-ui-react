91ecda6f7489da8d1c1781452b01fdc692ea0428
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-23 10:52:45 +0530

    add header

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Landing from "./Landing";
+
 import Container from "@material-ui/core/Container";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Slide from "@material-ui/core/Slide";
 
 import { DataProvider } from "../context/data.context";
 import { AttributeProvider } from "../context/attribute.context";
@@ -9,10 +16,30 @@ import useStyles from "../styles/AppStyles";
 
 function App() {
   const classes = useStyles();
+
+  function HideOnScroll(props) {
+    const { children } = props;
+    const trigger = useScrollTrigger();
+    return (
+      <Slide appear={false} direction="down" in={!trigger}>
+        {children}
+      </Slide>
+    );
+  }
+
   return (
     <DataProvider>
       <AttributeProvider>
         <HistoricalDataProvider>
+          <CssBaseline />
+          <HideOnScroll>
+            <AppBar>
+              <Toolbar>
+                <Typography variant="h6">NavBar</Typography>
+              </Toolbar>
+            </AppBar>
+          </HideOnScroll>
+          <Toolbar />
           <Container className={classes.root} maxWidth="lg">
             <Landing />
           </Container>
