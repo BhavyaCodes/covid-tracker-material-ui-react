155d9ee9b1a0a9cec18fda310770239dd1207b5c
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-23 09:58:53 +0530

    update padding-2

--- src/styles/LandingStyles.js
+++ src/styles/LandingStyles.js
@@ -3,16 +3,16 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
   root: {},
   left: {
-    paddingLeft: theme.spacing(3),
-    paddingRight: theme.spacing(3),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
     [theme.breakpoints.down("sm")]: {
       paddingLeft: "0",
       paddingRight: "0",
     },
   },
   right: {
-    paddingLeft: theme.spacing(3),
-    paddingRight: theme.spacing(3),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
   },
   container: {
     [theme.breakpoints.up("sm")]: {
