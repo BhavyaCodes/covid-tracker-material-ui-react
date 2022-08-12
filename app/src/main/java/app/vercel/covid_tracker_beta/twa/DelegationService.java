b4d7155052ebff54e02bb344d1310c5bb9930e1d
BhavyaCodes <xbhavyatomar@gmail.com>
2022-08-13 00:19:44 +0530

    add assetLinks.json

---
+++ app/src/main/java/app/vercel/covid_tracker_beta/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.covid_tracker_beta.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
